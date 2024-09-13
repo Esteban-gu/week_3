05d949442da888d714ebf45c3bd2f2c5a3f8c580
Alvarez-Gutierrez <ealvarez-gu@cps.k12.il.us>
2024-09-13 12:27:40 -0500

    update this shit now

---
+++ testColab.py
@@ -0,0 +1,15 @@
+# this is just for testing
+# for collaborative purposes
+#create an input for the user to input their age
+#create an input for the user to input their name
+#create an input for the user to input their favorite song
+
+
+
+
+name= input("What is your name?")
+print("Ok,", name) 
+commission= input("How much have you sold this month?")
+print("Your amount of commissions entitiled to you:", int(commission)*13/100)
+
+#Made by: Esteban, Karol, Orlando, and Jesse
